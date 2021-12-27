bde297fc05ce94f4b38b7bedc016750528d688a2
milagrosfl <96752226+milagrosfl@users.noreply.github.com>
2021-12-27 18:12:18 -0500

    cambios
    cambios

---
+++ src/app/app-navigation.ts
@@ -0,0 +1,141 @@
+// export const navigation = [
+//   {
+//     text: 'Home',
+//     path: '/home',
+//     icon: 'home'
+//   },
+//   {
+//     text: 'Examples',
+//     icon: 'folder',
+//     items: [
+//       {
+//         text: 'Profile',
+//         path: '/profile'
+//       },
+//       {
+//         text: 'Profile',
+//         path: ''
+//       },
+//       {
+//         text: 'xxxxx',
+//         path: ''
+//       },
+//       {
+//         text: 'Tasks',
+//         path: '/tasks'
+//       }
+//     ]
+//   },
+//   {
+//     text: 'Examples1',
+//     icon: 'folder',
+//     items: [
+//       {
+//         text: 'cccc',
+//         path: ''
+//       },
+//       {
+//         text: 'Profile',
+//         path: ''
+//       },
+//       {
+//         text: 'xxxxx',
+//         path: ''
+//       }
+//     ]
+//   },
+//   {
+//     text: 'Examples3',
+//     icon: 'folder',
+//     items: [
+//       {
+//         text: 'cccc',
+//         path: ''
+//       },
+//       {
+//         text: 'Profile',
+//         path: ''
+//       },
+//       {
+//         text: 'xxxxx',
+//         path: ''
+//       }
+//     ]
+//   },
+//   {
+//     text: 'Examples3',
+//     icon: 'folder',
+//     items: [
+//       {
+//         text: 'cccc',
+//         path: ''
+//       },
+//       {
+//         text: 'Profile',
+//         path: ''
+//       },
+//       {
+//         text: 'xxxxx',
+//         path: ''
+//       }
+//     ]
+//   },
+//   {
+//     text: 'Examples4',
+//     icon: 'folder',
+//     items: [
+//       {
+//         text: 'cccc',
+//         path: ''
+//       },
+//       {
+//         text: 'Profile',
+//         path: ''
+//       },
+//       {
+//         text: 'xxxxx',
+//         path: ''
+//       }
+//     ]
+//   }
+// ];
+
+
+export const navigation = [
+  {
+    id: '1',
+    text: 'Home',
+    path: '/home',
+    icon: 'home'
+  },
+  {
+  id: '2',
+  text: 'Stores',
+  path: '',
+  icon: 'home',
+  items: [{
+    id: '2_1',
+    text: 'Walters',
+    items: [{
+        id: '1_1_1_2',
+        text: 'Projector Plus',
+      }, {
+        id: '2_1_1_3',
+        text: 'Projector PlusHD',      
+    }],
+  },
+  {
+    id: '2_2',
+    text: 'Console',
+    path: '',
+  }],
+}];
+
+
+
+
+
+
+
+
+
